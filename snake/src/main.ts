9d781250552991dd24cb7970a7bd7b8d1c14754b
lebrancconvas <lebrancconvas@gmail.com>
2022-03-24 02:47:06 +0700

    🔨 Delete Physics in Config Variable.

--- snake/src/main.ts
+++ snake/src/main.ts
@@ -1,18 +1,12 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
-import SnakeScene from './scenes/SnakeScene'
+import SnakeScene from './scenes/SnakeScene';
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: window.innerWidth - 20,
-	height: window.innerHeight - 20,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
+	width: 800,
+	height: 600,
 	scene: [SnakeScene]
-}
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
